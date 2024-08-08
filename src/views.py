018488a40ee71d889729ad9421fe657bd6626a35
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-08 18:28:47 +0600

    editing get_total_amount_expenses and add show_cashback

--- src/views.py
+++ src/views.py
@@ -2,13 +2,13 @@ import datetime
 from get_data import get_data_from_excel, PATH_TO_FILE_EXCEL
 
 
-# Текущее время
-current_time = str(datetime.datetime.now())[11:16]
-
-def say_hello(time_of_day):
+def say_hello():
     """ Функция принимает строку со временем и здоровается в зависимости от времени суток """
+    # Текущее время
+    current_time = str(datetime.datetime.now())[11:16]
+
     # Получаем часы и переводим их в число
-    time = int(time_of_day[:2])
+    time = int(current_time[:2])
 
     # В зависимости от времени здороваемся
     if 6 <= time <= 11:
@@ -24,7 +24,7 @@ def say_hello(time_of_day):
 
 
 if __name__ == '__main__':
-    print(say_hello(current_time))
+    print(say_hello())
 
 def mask_card_number(number_card):
     """ Маскирует номер карты(показывает 4 последние цифры) """
@@ -44,18 +44,27 @@ if __name__ == "__main__":
     print(mask_card_number(7000792289606361))
 
 
-def get_total_amount_expenses(transactions):
+def get_total_amount_expenses(transactions, number_card):
     """ Общая сумма расходов """
-    numbers_card =[]
+    # Список, в который попадают все операции по карте
+    sum_list = []
     for tr in transactions:
-        if tr.get("Номер карты"):
-            numbers_card.append(tr.get("Номер карты"))
-    return set(numbers_card)
+        # Проверяем - операция относится к данной карте, статус - ок, не входит в указанные категории и со знаком минус
+        if str(tr.get("Номер карты"))[-4:] == str(number_card)[-4:] and tr.get("Статус") == "OK" and tr.get("Категория") not in ["Переводы", "Пополнения", "Другое", "Бонусы"] and float(tr.get("Сумма платежа")) < 0:
+            sum_list.append(tr.get("Сумма платежа"))
+    sum_list = sum(sum_list)
+    return -sum_list
 
 
-if __name__ == '__main__':
-    print(get_total_amount_expenses(get_data_from_excel(PATH_TO_FILE_EXCEL)))
+# if __name__ == '__main__':
+#     print(get_total_amount_expenses(get_data_from_excel(PATH_TO_FILE_EXCEL), "**7197"))
 
-def show_cashback(transactions):
+def show_cashback(expenses):
     """ Функция считает кэшбэк(1 рубль за каждые 100 рублей) """
-    pass
+    # Округляем кэшбэк до двух цифр после запятой
+    cashback = round(expenses * 0.01, 2)
+    return cashback
+
+
+# if __name__ == '__main__':
+#     print(show_cashback(get_total_amount_expenses(get_data_from_excel(PATH_TO_FILE_EXCEL), "**7197")))
